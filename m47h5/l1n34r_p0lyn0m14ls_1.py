790c77f20a3188059723cc985cf6223fbe140fc0
chillax504 <chillax.shastra@protonmail.com>
2022-05-24 18:06:04 +0000

    com1

---
+++ m47h5/l1n34r_p0lyn0m14ls_1.py
@@ -0,0 +1,22 @@
+import re
+
+exp = input("expression : ")
+coeffs = []
+
+op = ''
+for i in exp.split(' '):
+    if i == '-' or i == '+' or i == '/' or i == '*':
+        op = i
+    elif not op=='' :
+        coeffs.append(str(op)+str(i))
+        op = ''
+    else :
+        coeffs.append(i)
+
+for i in coeffs:
+    if any(c.isalpha() for c in i):
+        coeff = int(i[:-1])
+    else :
+        const = int(i.replace('+',''))
+
+print((0-const)/coeff)
