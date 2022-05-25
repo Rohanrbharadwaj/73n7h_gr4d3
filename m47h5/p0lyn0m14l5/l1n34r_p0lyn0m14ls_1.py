459d387c0ad7516e45f4b06f3c3fa496d6557b3a
Rohan R Bharadwaj <rohanrbharadwaj@gmail.com>
2022-05-26 00:59:40 +0530

    "prettification"

--- m47h5/p0lyn0m14l5/l1n34r_p0lyn0m14ls_1.py
+++ m47h5/p0lyn0m14l5/l1n34r_p0lyn0m14ls_1.py
@@ -1,23 +1,29 @@
-#this program finds the zero of a linear polynomial
-#please use spacing between terms 
+# This program finds the zero of a linear polynomial
+# Please use spacing between terms 
 
-exp = input("expression : ")
-coeffs = []
-op = ''
+def eval_linear(expression: str) -> int:
+    coeffs = []
+    op = ''
 
-for i in exp.split(' '):
-    if i == '-' or i == '+' or i == '/' or i == '*':
-        op = i
-    elif not op=='' :
-        coeffs.append(str(op)+str(i))
-        op = ''
-    else :
-        coeffs.append(i)
+    for i in exp.split(' '):
+        if i == '-' or i == '+' or i == '/' or i == '*':
+            op = i
+        elif not op=='' :
+            coeffs.append(str(op)+str(i))
+            op = ''
+        else :
+            coeffs.append(i)
 
-for i in coeffs:
-    if any(c.isalpha() for c in i):
-        coeff = int(i[:-1])
-    else :
-        const = int(i.replace('+',''))
+    for i in coeffs:
+        if any(c.isalpha() for c in i):
+            if len(i) == 1:
+                coeff = 1
+            else:
+               coeff = int(i[:-1])           
+        else :
+            const = int(i.replace('+',''))
 
-print((0-const)/coeff)
+    return (0-const)/coeff
+
+exp = input("Expression : ")
+print(eval_linear(exp))
