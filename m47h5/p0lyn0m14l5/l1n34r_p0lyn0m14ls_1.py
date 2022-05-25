76f64810ae512c4f43803df26024d057da202f37
Rohan R Bharadwaj <rohanrbharadwaj@gmail.com>
2022-05-26 01:05:48 +0530

    black formatting

--- m47h5/p0lyn0m14l5/l1n34r_p0lyn0m14ls_1.py
+++ m47h5/p0lyn0m14l5/l1n34r_p0lyn0m14ls_1.py
@@ -1,17 +1,18 @@
 # This program finds the zero of a linear polynomial
-# Please use spacing between terms 
+# Please use spacing between terms
+
 
 def eval_linear(expression: str) -> int:
     coeffs = []
-    op = ''
+    op = ""
 
-    for i in exp.split(' '):
-        if i == '-' or i == '+' or i == '/' or i == '*':
+    for i in exp.split(" "):
+        if i == "-" or i == "+" or i == "/" or i == "*":
             op = i
-        elif not op=='' :
-            coeffs.append(str(op)+str(i))
-            op = ''
-        else :
+        elif not op == "":
+            coeffs.append(str(op) + str(i))
+            op = ""
+        else:
             coeffs.append(i)
 
     for i in coeffs:
@@ -19,11 +20,12 @@ def eval_linear(expression: str) -> int:
             if len(i) == 1:
                 coeff = 1
             else:
-               coeff = int(i[:-1])           
-        else :
-            const = int(i.replace('+',''))
+                coeff = int(i[:-1])
+        else:
+            const = int(i.replace("+", ""))
 
-    return (0-const)/coeff
+    return (0 - const) / coeff
+
 
 exp = input("Expression : ")
 print(eval_linear(exp))
