d9525cb56e6ec490c2784b9b690fd97c669fada0
Rohan R Bharadwaj <rohanrbharadwaj@gmail.com>
2022-05-26 01:53:26 +0530

    chillax, bruv.

--- m47h5/p0lyn0m14l5/qu4dr471c_p0lyn0m14l5_1.py
+++ m47h5/p0lyn0m14l5/qu4dr471c_p0lyn0m14l5_1.py
@@ -1,54 +1,64 @@
-from cmath import sqrt
+# This program finds the zeroes of a quadratic polynomial, if they just so happen to be real
+# Please use spacing between terms
+
+def eval_quadratic(exp: str) -> None:
+    if "^2" not in expression:
+        print("Expression's not quadratic")
+
+    terms = []
+    op = ""
+    coeff_b = const = 0
+
+    for i in exp.split(" "):
+        if i == "-" or i == "+":
+            op = i
+        elif op:
+            terms.append(str(op) + str(i))
+            op = ""
+        else:
+            terms.append(i)
+    print()
+
+    for i in terms:
+        if not any(c.isalpha() for c in i):
+            const = int(i)
+        elif "^2" in i:
+            if len(i) == 3:
+                coeff_a = 1
+            elif i[:-3] == "-":
+                coeff_a = -1
+            else:
+                coeff_a = int(i.replace("+", "")[:-3])
+        else:
+            if i[:-1] == "+":
+                coeff_b = 1
+            elif i[:-1] == "-":
+                coeff_b = -1
+            else:
+                coeff_b = int(i[:-1])
+
+    # If a quadratic polynomial is give by ax^2 +  bx + c
+    # sum of roots = -b/a
+    # product of roots = c/a
+    # zeroes = (-b +- sqrt(b^2 - 4*a*c)) / 2*coeffa
+
+    discriminant = (coeff_b) ** 2 - (4 * coeff_a * const)
+    print("Discriminant = {}".format(discriminant))
+
+    if discriminant == 0:
+        zero = -coeff_b / (2 * coeff_a)
+        print("The zeroes are real and identitical")
+        print("α = β = {}".format(zero))
+    elif discriminant > 0:
+        rooted = discriminant ** (0.5)
+        alpha = (-coeff_b + rooted) / (2 * coeff_a)
+        beta = (-coeff_b - rooted) / (2 * coeff_a)
+        print("The zeroes are real and unique")
+        print("α = {}".format(alpha))
+        print("β = {}".format(beta))
+    else:
+        print("The zeroes are imaginary")
 
 
-exp = input("expression : ")
-coeffs = []
-op = ''
-
-for i in exp.split(' '):
-    if i == '-' or i == '+' or i == '/' or i == '*':
-        op = i
-    elif not op=='' :
-        coeffs.append(str(op)+str(i))
-        op = ''
-    else :
-        coeffs.append(i)
-print(coeffs)
-
-for i in coeffs:
-    if not any(c.isalpha() for c in i):
-        const = int(i)
-    elif any(c.isalpha() for c in i):
-        if '^2' in i :
-            if i.replace('+','')[:-3] == '' :
-                coeffa = 1
-            else :
-                coeffa = int(i.replace('+','')[:-3])
-        else :
-            if i.replace('+','')[:-3] == '' :
-                coeffb = 1
-            else :
-                coeffb = int(i[:-1])
-
-alph = 0
-beta = 0
-
-#alpha + beta = -b/a
-#alpha * beta = c/a
-#zero = (-b +- sqrt(coeffb^2 - 4*coeffa*const))/2*coeffa
-
-reality = coeffb^2 - 4 * coeffa * const
-
-if reality == 0 :
-    zero = - coeffb / 2 * coeffa
-    print("\n the zeroes are real and identitical")
-    print("α = β = ", zero)
-elif reality > 0 :
-    rooted = sqrt(abs(reality))
-    alph = - coeffb + rooted / 2 * coeffa
-    beta = - coeffb - rooted / 2 * coeffa
-    print("\n the zeroes are real and unique")
-    print("α = ", alph)
-    print("β = ", beta)
-else :
-    print("the zeroes are unreal")
+expression = input("Expression : ")
+eval_quadratic(expression)
